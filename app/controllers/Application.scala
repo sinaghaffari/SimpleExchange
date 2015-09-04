c67ff96f66652847e1801f94fa17b44ed79f5c13
Sina Ghaffari <sina.ghaffari321@gmail.com>
2015-09-04 02:42:54 -0400

    Overhauled the look and feel. Added a button to swap the source and destination currencies. changed the way the currencies are displayed.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,23 +12,22 @@ object Application extends Controller {
     import play.api.libs.json.{JsValue, JsObject}
     import play.api.libs.ws._
     import play.api.Play.current
-    implicit val context = play.api.libs.concurrent.Execution.Implicits.defaultContext;
-    var dstAmt_ = dstAmt;
-    val futureCurrenciesResponse = WS.url("http://openexchangerates.org/currencies.json").get().map { response => response.json };
+    implicit val context = play.api.libs.concurrent.Execution.Implicits.defaultContext
+    var dstAmt_ = dstAmt
+    val futureCurrenciesResponse = WS.url("http://openexchangerates.org/currencies.json").get().map { response => response.json }
     val resultCurrenciesResponse : Try[JsValue] = Await.ready(futureCurrenciesResponse, Duration.Inf).value.get
-    val currencies = resultCurrenciesResponse.get.asInstanceOf[JsObject];
+    val currencies = resultCurrenciesResponse.get.asInstanceOf[JsObject]
 
-    val futureResult = WS.url("http://openexchangerates.org/latest.json?app_id=7f09b6b1b34b4642888c99cd87847c3b").get().map { response => response.json};
+    val futureResult = WS.url("http://openexchangerates.org/latest.json?app_id=7f09b6b1b34b4642888c99cd87847c3b").get().map { response => response.json }
     val result = Await.ready(futureResult, Duration.Inf).value.get
-    val latestResponse = result.get.asInstanceOf[JsObject];
-    val latestRates = latestResponse.value.get("rates").get.asInstanceOf[JsObject];
+    val latestResponse = result.get.asInstanceOf[JsObject]
+    val latestRates = latestResponse.value.get("rates").get.asInstanceOf[JsObject]
     if (latestRates.keys.contains(srcCur) && latestRates.keys.contains(dstCur)) {
-      val srcCurVal = latestRates.value.get(srcCur).get.toString().toFloat;
-      val dstCurVal = latestRates.value.get(dstCur).get.toString().toFloat;
-      val conversionRate = dstCurVal/srcCurVal;
-      dstAmt_ = srcAmt * conversionRate;
+      val srcCurVal = latestRates.value.get(srcCur).get.toString().toFloat
+      val dstCurVal = latestRates.value.get(dstCur).get.toString().toFloat
+      val conversionRate = dstCurVal / srcCurVal
+      dstAmt_ = srcAmt * conversionRate
     }
-
     Ok(views.html.index(currencies, srcAmt, dstAmt_, srcCur, dstCur))
   }
 }
---
+++ public/javascript/main.js
@@ -0,0 +1,51 @@
+// jshint devel:true
+jQuery.fn.center = function(parent) {
+    if (parent) {
+        parent = this.parent();
+    } else {
+        parent = window;
+    }
+    this.css({
+        "position": "absolute",
+        //"top": ((($(parent).height() - this.outerHeight()) / 2) + $(parent).scrollTop() + "px"),
+        "left": ((($(parent).width() - this.outerWidth()) / 2) + $(parent).scrollLeft() + "px")
+    });
+    return this;
+}
+$(document).ready(function() {
+    $('select').select2({theme: "bootstrap", width: "resolve"});
+    $('.select2-selection')
+        .css("border-radius", "0px")
+        .css("height", "34px")
+        .css("margin-left", "0px")
+        .css("border-left-width", "0px")
+        .addClass("conversion-form");
+    $('.select2-selection__rendered').addClass("conversion-form");
+    $('#srcCur').on("select2:open", function(e) {
+        $('.select2-dropdown')
+            .addClass("conversion-form")
+            .css("width", "+=1")
+            .css("margin-left", "-1px");
+    });
+    $('#dstCur').on("select2:open", function(e) {
+        $('.select2-dropdown')
+            .addClass("conversion-form")
+            .css("width", "+=1")
+            .css("margin-left", "-1px");
+        $('.select2-results__option[aria-selected=true]')
+            .css("background-color", "#1abc9c")
+            .css("color", "#ecf0f1");
+
+    });
+    $(window).resize( function() {
+        $('#displayArea').center();
+    });
+    $('#swap-button').click(function() {
+        var dstCur = $('#dstCur').val();
+        var srcCur = $('#srcCur').val();
+        $('#dstCur').val(srcCur).trigger("change");
+        $('#srcCur').val(dstCur).trigger("change");
+        console.log(dstCur + " <-> " + srcCur);
+    });
+    $('#displayArea').center();
+});
