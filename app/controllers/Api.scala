e11a4df3032c7dc7795b59f2097b486f88f34c35
sinaghaffari <sina.ghaffari321@gmail.com>
2015-11-21 19:02:14 -0500

    Test API

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -4,6 +4,14 @@ package controllers
   * Created on 2015-11-13.
   * @author Sina Ghaffari
   */
-object Api {
+import play.api.mvc._
 
+import scala.concurrent.Future
+
+object Api extends Controller {
+  def index(srcAmtS: String = "", srcCur: String = "", dstCur: String = "") = Action.async { request =>
+    Future {
+      Ok("shit's good!");
+    }
+  }
 }
